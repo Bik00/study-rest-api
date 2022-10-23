3b5528204e599561e28a0e67dc2b685769f3f6c4
bikoo <bikoo3002@naver.com>
2022-10-23 19:38:30 +0900

    테스트 코드 1 작성 : 성공적으로 이벤트 생성 시, 201 응답 코드를 반환

--- src/test/java/com/study/restapi/events/controller/EventControllerTest.java
+++ src/test/java/com/study/restapi/events/controller/EventControllerTest.java
@@ -1,5 +1,7 @@
 package com.study.restapi.events.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.restapi.events.Event;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = {EventController.class})
@@ -17,13 +23,34 @@ class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     @DisplayName("성공적으로 이벤트 생성 시, 201 응답 코드를 반환합니다.")
     public void createEvent() throws Exception {
-        mockMvc.perform(post("/api/events/")
+
+        // Given
+        Event event = Event.builder()
+                .name("spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.now().withMinute(0).withSecond(0))
+                .closeEnrollmentDateTime(LocalDateTime.now().withMinute(59).withSecond(59))
+                .beginEventDateTime(LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0))
+                .endEventDateTime(LocalDateTime.now().plusDays(1).withHour(23).withMinute(59).withSecond(59))
+                .basePrice(100)
+                .basePrice(200)
+                .limitOfEnrollment(100)
+                .location("서울대 입구역 스터디 카페")
+                .build();
+
+        // When
+        mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .accept(MediaTypes.HAL_JSON)    // HAL 적용
-                )
-                .andExpect(status().isCreated());
+                        .accept(MediaTypes.HAL_JSON) // HAL 적용
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
