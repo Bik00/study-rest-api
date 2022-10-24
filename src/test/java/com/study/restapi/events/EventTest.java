c426e26e4ad633b928957837bb7400a8e516bb65
bikoo <bikoo3002@naver.com>
2022-10-24 22:12:12 +0900

    파라미터 입출력 테스트 (JUnitParams 사용)

--- src/test/java/com/study/restapi/events/EventTest.java
+++ src/test/java/com/study/restapi/events/EventTest.java
@@ -1,11 +1,20 @@
 package com.study.restapi.events;
 
-import org.hibernate.annotations.DialectOverride;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 class EventTest {
 
     @Test
@@ -36,58 +45,50 @@ class EventTest {
         assertThat(event.getDescription()).isEqualTo(descriptioin);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 100, false"
+    })
     @DisplayName("basePrice와 maxPrice가 0이면 무료, 아니면 유료 입니다.")
-    public void testFree() {
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
 
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isFree()).isTrue();
-
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse();
+        assertThat(event.isFree()).isEqualTo(isFree);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("isOffline")
     @DisplayName("basePrice와 maxPrice가 0이면 무료, 아니면 유료 입니다.")
-    public void testOffline() {
+    public void testOffline(String location, boolean isOffline) {
 
         // Given
         Event event = Event.builder()
-                .location("대구광역시 북구 복현로")
+                .location(location)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
 
-        // Given
-        event = Event.builder()
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isOffline()).isFalse();
+    private static Stream<Arguments> isOffline() {
+        return Stream.of(
+                Arguments.of("강남역", true),
+                Arguments.of(null, false),
+                Arguments.of("", false)
+        );
     }
 }
